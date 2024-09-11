525426bf41e449d475f76b4d4c398e667011e0c1
JIMYUNG_YU <jm_7516@naver.com>
2024-09-11 18:30:21 +0900

    0911 BACK, FRONT 생성

---
+++ BACK/src/main/java/com/proejct/cooperation/controller/HelloController.java
@@ -0,0 +1,16 @@
+package com.proejct.cooperation.controller;
+
+import lombok.Getter;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public ResponseEntity<?> hello(){
+        return ResponseEntity.status(200).body("hello");
+    }
+}
---
+++ front/src/App.js
@@ -0,0 +1,38 @@
+import logo from './logo.svg';
+import './App.css';
+import { useEffect, useState } from 'react';
+
+function App() {
+
+  const [data, setData] = useState("");
+
+  useEffect(()=>{
+    const fetchData = async () => {
+      try {
+        const res = await fetch("/hello", {
+          method: "GET",
+          headers: {
+            "content-type": "application/json",
+          },
+        });
+        console.log(res);
+        const result = await res.text();  // 응답을 JSON으로 파싱
+
+        setData(result);  // 받은 데이터를 상태로 저장
+        console.log(result);
+      } catch (e) {
+        console.log(e);  // 에러가 발생하면 콘솔에 출력
+      }
+    };
+    
+    fetchData();  // 함수 호출
+  }, []);  // 의존성 배열을 추가하여 컴포넌트가 마운트될 때 한 번만 실행
+
+  return (
+    <div className="App">
+      {data}
+    </div>
+  );
+}
+
+export default App;
